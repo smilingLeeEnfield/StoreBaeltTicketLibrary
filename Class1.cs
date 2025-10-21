fa15d46573f0559b931cb5692930b265553405c0
smilingLeeEnfield <159907717+smilingLeeEnfield@users.noreply.github.com>
2025-10-21 10:20:39 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StorebæltCar : bridge.Car
+    {
+        public StorebæltCar(string LP, bool biz) : base(LP, biz)
+        {
+        }
+        /// <summary>
+        /// returns price of a car ticket on the Storebælt bridge with a discount if brobizz is used and a weekend discount
+        /// </summary>
+        /// <returns></returns>
+        public override double Price()
+        {
+            double tPrice = base.Price();
+            if (DateTime.Now.DayOfWeek == 0 || DateTime.Now.DayOfWeek == 0) return base.Price() *0.85;
+            return tPrice;
+        }
+    }
+}
---
+++ RepositoryClass.cs
@@ -0,0 +1,40 @@
+﻿using bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class RepositoryClass
+    {
+        public static List<StorebæltCar> CarList= new List<StorebæltCar>();
+
+        /// <summary>
+        /// Returns a list of all cars in the repository
+        /// </summary>
+        /// <returns></returns>
+        public List<StorebæltCar> GetAllCars()
+        {
+            return CarList;
+        }
+        /// <summary>
+        /// Adds a car to the repository
+        /// </summary>
+        /// <param name="car"></param>
+        public void AddCar(StorebæltCar car)
+        {
+            CarList.Add(car);
+        }
+        /// <summary>
+        /// Finds all tickets by licence plate
+        /// </summary>
+        /// <param name="lp"></param>
+        /// <returns></returns>
+        public List<StorebæltCar> TicketByLP(string lp)
+        {
+            return CarList.FindAll(i=>i.LicencePlate==lp).ToList();
+        }
+    }
+}
